25fb406f27a369736e54c51b68e5e5149d49745b
Filip Finfando <filip.finfando@gmail.com>
2018-11-01 21:50:04 +0100

    Some work on AverageSpeedControl, but got stucked at custom window function.

--- src/main/java/master2018/flink/SpeedRadar.java
+++ src/main/java/master2018/flink/SpeedRadar.java
@@ -8,7 +8,7 @@ public class SpeedRadar implements FilterFunction<Tuple8<Integer, Integer, Integ
 	@Override
 	public boolean filter(Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer> in)
 			throws Exception {
-		System.out.println("Velocidad "+ in.f2);
+//		System.out.println("Velocidad "+ in.f2);
 		if (in.f2 > 90) {
 			return true;
 		} else {
--- src/main/java/master2018/flink/VehicleTelematics.java
+++ src/main/java/master2018/flink/VehicleTelematics.java
@@ -1,14 +1,14 @@
 package master2018.flink;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.tuple.Tuple6;
-import org.apache.flink.api.java.tuple.Tuple8;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
+import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class VehicleTelematics {
     public static void main(String[] args) throws Exception {
@@ -20,17 +20,26 @@ public class VehicleTelematics {
         SingleOutputStreamOperator<Tuple6<Integer,Integer,Long,Integer,Integer,Integer>> speedRadar = mapStream.filter(new SpeedRadar()).map(new FlatMapOutput());
         speedRadar.writeAsCsv(args[1] + "/speedfines.csv", FileSystem.WriteMode.OVERWRITE);
 
-//        SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>> averageSpeedControl = mapStream;
-//        averageSpeedControl.writeAsCsv(args[1] + "/avgspeedfines.csv", FileSystem.WriteMode.OVERWRITE);
-//
+        SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>> filteredStream = mapStream
+                .filter(new FilterFunction<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>>() {
+                    @Override
+                    public boolean filter(Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer> in) throws Exception {
+                        if (in.f6 >= 52 && in.f6 <= 56) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                });
+//        KeyedStream<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>, Tuple> keyedStream = filteredStream.keyBy(1);
+//        SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>> result = keyedStream
+//                .window(ProcessingTimeSessionWindows.withGap(Time.milliseconds(10)))
+//                .apply(new AverageSpeedControl());
+        filteredStream.writeAsCsv(args[1] + "/avgspeedfines.csv", FileSystem.WriteMode.OVERWRITE);
+
+
 //        SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Long, Integer, Boolean, Integer, Integer>> accidentReporter = mapStream;
 //        accidentReporter.writeAsCsv(args[1] + "/accidents.csv", FileSystem.WriteMode.OVERWRITE);
-
-
-
-
-
-//        flatMapOut.writeAsCsv(outFilePath, FileSystem.WriteMode.OVERWRITE);
 
         try {
             env.execute();
@@ -40,3 +49,4 @@ public class VehicleTelematics {
 
     }
 }
+
