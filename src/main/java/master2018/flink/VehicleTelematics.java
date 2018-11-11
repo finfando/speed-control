c1dfca04245403eec3e0f114e3e55bb68cfb7e89
Filip Finfando <filip.finfando@gmail.com>
2018-11-11 20:36:59 +0100

    some experiments, but not very successful

--- src/main/java/master2018/flink/VehicleTelematics.java
+++ src/main/java/master2018/flink/VehicleTelematics.java
@@ -33,32 +33,31 @@ public class VehicleTelematics {
 				.filter(new SpeedRadar()).map(new FlatMapOutput());
 		speedRadar.writeAsCsv(outFilePath + "/speedfines.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 
-		SingleOutputStreamOperator<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> filteredStream = mapStream
-                .map(new MapFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>>() {
-                    private static final long serialVersionUID = 1L;
-                    @Override
-                    public Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> map(
-                            Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> in)
-                            throws Exception { // time vid xway seg dir pos
-                        return new Tuple6<>(in.f0, in.f1, in.f3, in.f5, in.f6, in.f7); // T8 Time0, VID1,Spd2, XWay3,
-                        // Lane4,Dir5, Seg6, Pos7
-                    }
-                }).setParallelism(1)
+		SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> filteredStream = mapStream
                 .filter(new FilterBySegment()).setParallelism(1)
                 ;
 
 		KeyedStream<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>, Tuple> keyedStream = filteredStream
 				.assignTimestampsAndWatermarks(
-						new AscendingTimestampExtractor<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>>() {
+						new AscendingTimestampExtractor<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
 							private static final long serialVersionUID = 1L;
 
 							@Override
 							public long extractAscendingTimestamp(
-									Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> element) {
+									Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> element) {
 								return element.f0 * 1000;
 							}
-						})
-				.setParallelism(1).keyBy(1);
+						}).setParallelism(1)
+                .map(new MapFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>>() {
+                    private static final long serialVersionUID = 1L;
+                    @Override
+                    public Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> map(
+                            Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> in)
+                            throws Exception {
+                        return new Tuple6<>(in.f0, in.f1, in.f3, in.f5, in.f6, in.f7);
+                    }
+                }).setParallelism(1)
+                .keyBy(1, 3);
 
 		SingleOutputStreamOperator<Tuple6<Integer, Integer, Integer, Integer, Integer, Double>> avgSpeedControl = keyedStream
 				.window(EventTimeSessionWindows.withGap(Time.seconds(180))).apply(new AverageSpeedControl());
--- src/main/java/master2018/flink/utils/FilterBySegment.java
+++ src/main/java/master2018/flink/utils/FilterBySegment.java
@@ -1,14 +1,14 @@
 package master2018.flink.utils;
 
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.java.tuple.Tuple6;
+import org.apache.flink.api.java.tuple.Tuple8;
 
 public class FilterBySegment
-		implements FilterFunction<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> {
+		implements FilterFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> {
 	private static final long serialVersionUID = 1L;
 	@Override
-	public boolean filter(Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> in)
+	public boolean filter(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> in)
 			throws Exception {
-		return in.f4 >= 52 && in.f4 <= 56;
+		return in.f6 >= 52 && in.f6 <= 56;
 	}
 }
